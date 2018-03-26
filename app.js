1d1b2f7e6ee18e71276b4aa91ec0a42d612d4f27
nattyayana <natnalezayana@gmail.com>
2018-03-26 13:33:40 -0700

    started on computer and player selections functions and variables

---
+++ app.js
@@ -0,0 +1,30 @@
+var options = ['rock', 'paper', 'scissors'];
+
+var round = 0
+
+var lives = 5; 
+
+var playerChoice = ;
+var computerChoice = Math.random();
+
+// var timeMachine = false;
+// var cannibal = false;
+
+
+
+if (computerChoice <.33) { 
+
+  selectedChoice = 'rock'; 
+
+ }
+
+ else if  (computerChoice >.67) {
+
+  selectedChoice = 'paper';
+
+ }
+    
+
+else {selectedChoice = 'scissors';}
+console.log(selectedChoice); 
+
